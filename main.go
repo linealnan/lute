248f630c9db2f96355336b843f402bc4465ad1a2
Dmitriy Shkatov <Dmitriy.Shkatov@waveaccess.ru>
2020-11-12 11:54:23 +0500

    [NF] Add logo

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+const (
+	luteVersion = "1.0"
+	luteLogo    = `
+
+    MMMMF                   mm             
+    MM                      MM             
+    MM          MM    MM  mmMMmm   .gP"Ya  
+    MM          MM    MM    MM    ,M'   Yb 
+    MM          MM    MM    MM    8M"""""" 
+    MM       M  MM    MM    MM    YM.    g 
+    JMMmmmmMMM  Mbod"YML.   Mbmo   Mbmmd2
+`
+)
+
+func main() {
+    fmt.Println(luteLogo)
+}
